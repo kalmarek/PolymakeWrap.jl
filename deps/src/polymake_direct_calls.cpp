1fce053133a406ea8d19efd51bf98eb846799898
kalmarek <kalmar@amu.edu.pl>
2019-11-28 17:27:39 +0100

    add floating-point solve_LP

--- deps/src/polymake_direct_calls.cpp
+++ deps/src/polymake_direct_calls.cpp
@@ -2,20 +2,30 @@
 
 #include "polymake_direct_calls.h"
 
-auto low_level_solve_LP(
-    pm::Matrix<pm::Rational> inequalities,
-    pm::Matrix<pm::Rational> equalities,
-    pm::Vector<pm::Rational> objective,
-    bool                     maximize)
+auto direct_call_solve_LP(
+    const pm::Matrix<pm::Rational>& inequalities,
+    const pm::Matrix<pm::Rational>& equalities,
+    const pm::Vector<pm::Rational>& objective,
+    bool                            maximize)
 {
-    auto solution = polymake::polytope::solve_LP<pm::Rational,
-    pm::Matrix<pm::Rational>, pm::Matrix<pm::Rational>, pm::Vector<pm::Rational>>
-    (inequalities, equalities, objective, maximize);
+    auto solution = polymake::polytope::solve_LP(inequalities, equalities, objective, maximize);
     return solution.solution;
 }
 
+auto direct_call_solve_LP_float(
+    const pm::Matrix<double>& inequalities,
+    const pm::Matrix<double>& equalities,
+    const pm::Vector<double>& objective,
+    bool                      maximize)
+{
+    auto solution = polymake::polytope::solve_LP(inequalities, equalities, objective, maximize);
+    return solution.solution;
+}
+
+
 void polymake_module_add_direct_calls(jlcxx::Module& polymake)
 {
-    polymake.method("low_level_solve_LP", &low_level_solve_LP);
+    polymake.method("direct_call_solve_LP", &direct_call_solve_LP);
+    polymake.method("direct_call_solve_LP_float", &direct_call_solve_LP_float);
     // polymake.method("low_level_solve_LP_direct", &polymake::polytope::solve_LP<pm::Rational, pm::Matrix<pm::Rational>, pm::Vector<pm::Rational>>);
 }
--- src/polymake_direct_calls.jl
+++ src/polymake_direct_calls.jl
@@ -1,20 +1,35 @@
+const AbstractRational = Union{Rational, pm_Rational}
+
 for (m, flag) in [(:min, false), (:max, true)]
     @eval begin
-        function low_levl_solve_LP(
-            inequalities::AbstractMatrix,
-            equalities::AbstractMatrix,
-            objective::AbstractVector, ::typeof($m))
+        function solve_LP(
+            inequalities::AbstractMatrix{<:AbstractRational},
+            equalities::AbstractMatrix{<:AbstractRational},
+            objective::AbstractVector{<:AbstractRational}, ::typeof($m))
 
-            return Polymake.low_level_solve_LP(
+            return direct_call_solve_LP(
                 convert(pm_Matrix{pm_Rational}, inequalities),
                 convert(pm_Matrix{pm_Rational}, equalities),
                 convert(pm_Vector{pm_Rational}, objective),
                 $flag
             )
         end
+
+        function solve_LP(
+            inequalities::AbstractMatrix{<:AbstractFloat},
+            equalities::AbstractMatrix{<:AbstractFloat},
+            objective::AbstractVector{<:AbstractFloat}, ::typeof($m))
+
+            return direct_call_solve_LP_float(
+                convert(pm_Matrix{Float64}, inequalities),
+                convert(pm_Matrix{Float64}, equalities),
+                convert(pm_Vector{Float64}, objective),
+                $flag
+            )
+        end
     end
 end
 
-solve_LP(inequalities, objective; sense=max) = low_levl_solve_LP(inequalities, pm_Matrix{pm_Rational}(), objective, sense)
+solve_LP(inequalities::AbstractMatrix{T}, objective::AbstractVector; sense=max) where T = solve_LP(inequalities, Matrix{T}(undef, 0, 0), objective, sense)
 
-solve_LP(inequalities, equalities, objective; sense=max) = low_levl_solve_LP(inequalities, equalities, objective, sense)
+solve_LP(inequalities::AbstractMatrix, equalities::AbstractMatrix, objective::AbstractVector; sense=max) = solve_LP(inequalities, equalities, objective, sense)
